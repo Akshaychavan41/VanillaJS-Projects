bc71d0b54f958e153736a37616f6c1f4ec6ed236
Akshaychavan41 <aschavan41@gmail.com>
2021-01-16 16:55:11 +0530

    FirstCommit

---
+++ TicketBooking/script.js
@@ -0,0 +1,29 @@
+const container = document.querySelector('.container');
+const seats = document.querySelectorAll('.row .seat:not(.occupied)');
+const count = document.getElementById('count');
+const total = document.getElementById('total');
+const movieSelect = document.getElementById('movie');
+var ticketPrice =  +movieSelect.value;
+//event listener
+
+function updateSelectedCount(){
+    const selectedSeats = document.querySelectorAll('.row .seat.selected');
+    const seatsCount = selectedSeats.length
+    count.innerText = seatsCount
+    total.innerText = seatsCount*ticketPrice
+}
+
+container.addEventListener('click', (e) => {
+    if (e.target.classList.contains('seat') && 
+        !e.target.classList.contains('occupied')) {
+        e.target.classList.toggle('selected')
+
+        updateSelectedCount()
+
+    }
+})
+
+movieSelect.addEventListener('click', (e) =>{
+    ticketPrice = e.target.value
+    updateSelectedCount()
+})
